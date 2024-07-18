301df59a464aa136172168e4b2e2bab87e2c56d0
@r0f33 <jiske.nicolson@gmail.com>
2024-07-18 17:02:50 +0930

    install express & nodemon & add scripts + boilerplate

---
+++ server/index.js
@@ -0,0 +1,12 @@
+// express boilerplate
+const express = require("express");
+const app = express();
+const port = 6001;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
