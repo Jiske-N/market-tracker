77ff393938d10a15d37d2edb79e7100d02ed7779
@r0f33 <jiske.nicolson@gmail.com>
2024-07-18 21:17:55 +0930

    add user to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,23 @@
-export const typeDefs = ``;
+export const typeDefs = `
+type User {
+_id: ID
+firstName: String
+lastName: String
+email: String
+darkMode: Boolean
+}
+
+type Auth {
+token: ID
+user: User
+}
+
+type Query {
+user: User
+}
+
+type Mutation {
+addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+updateUser(firstName: String, lastName: String, email: String, password: String, darkMode: Boolean): User
+login(email: String!, password: String!): Auth
+}`;
