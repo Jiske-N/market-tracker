6060970eb6dbc9c0d37e8bde51921f7378458d76
@r0f33 <jiske.nicolson@gmail.com>
2024-07-19 18:24:38 +0930

    add basic router dom to main.tsx and app.tsx & Welcome + Home Pages to test

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import Welcome from "./pages/Welcome.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        // error: <NoMatch/>,
+        children: [
+            {
+                index: true,
+                element: <Welcome />,
+            },
+            {
+                path: "/home",
+                element: <Home />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />)
