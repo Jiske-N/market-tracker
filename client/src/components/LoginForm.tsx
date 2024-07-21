b8142aead7bd34b27a332b3850dc6be80c29ca20
@r0f33 <jiske.nicolson@gmail.com>
2024-07-21 21:24:59 +0930

    create reusable container components. add container components to make layout of LoginSignUpStructure

---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,11 @@
+import ColumnFlexJustifyAlignCenter from "./ColumnFlexJustifyAlignCenter";
+
+const LoginForm = () => {
+    return (
+        <ColumnFlexJustifyAlignCenter>
+        <h1>Login form</h1>
+        </ColumnFlexJustifyAlignCenter>
+    )
+};
+
+export default LoginForm;
--- client/src/components/LoginSignupStructure.tsx
+++ client/src/components/LoginSignupStructure.tsx
@@ -1,7 +1,18 @@
+import ColumnFlexJustifyAlignCenter from "./ColumnFlexJustifyAlignCenter";
+import LoginForm from "./LoginForm";
+import RowFlexBetweenCenter from "./RowFlexBetweenCenter";
+
 const LoginSignupStructure = () => {
     return (
-        <h1>LoginSignupStructure</h1>
-    )
+        <RowFlexBetweenCenter>
+            <ColumnFlexJustifyAlignCenter flexGrow="2.2">
+                <h1>A cool background</h1>
+            </ColumnFlexJustifyAlignCenter>
+            <ColumnFlexJustifyAlignCenter flexGrow="1" minWidth="500px" minHeight='100vh'>
+                <LoginForm/>
+            </ColumnFlexJustifyAlignCenter>
+        </RowFlexBetweenCenter>
+    );
 };
 
 export default LoginSignupStructure;
