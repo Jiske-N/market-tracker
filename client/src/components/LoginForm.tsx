973d21443293b57e7f6a43e101a328f6f3e00758
@r0f33 <jiske.nicolson@gmail.com>
2024-07-22 21:00:19 +0930

    add resolver to login form

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -183,6 +183,7 @@
 // _______________________________________________________________________
 import React from "react"
 import { useForm, Resolver } from "react-hook-form"
+import { validateEmail, validatePassword } from "../utilities/helpers"
 
 type FormValues = {
   firstName: string
@@ -192,18 +193,60 @@ type FormValues = {
 }
 
 const resolver: Resolver<FormValues> = async (values) => {
-  return {
-    values: values.firstName ? values : {},
-    errors: !values.firstName
-      ? {
-          firstName: {
-            type: "required",
-            message: "First Name is required.",
-          },
-        }
-      : {},
+  let errors = {};
+  
+  if (!values.firstName) {
+    errors = {
+      ...errors,
+      firstName: {
+        type: "required",
+        message: "First Name is required.",
+      },
+    };
   }
-}
+
+  if (!values.lastName) {
+    errors = {
+      ...errors,
+      lastName: {
+        type: "required",
+        message: "Last Name is required.",
+      },
+    };
+  }
+
+  const validEmail: boolean = validateEmail(values.email);
+  if (!validEmail) {
+    errors = {
+      ...errors,
+      password: {
+        type: "required",
+        message: "Must be a valid email address.",
+      },
+    };
+  }
+
+  const validPassword: boolean = validatePassword(values.password);
+  if (!validPassword) {
+    errors = {
+      ...errors,
+      password: {
+        type: "required",
+        message: "Password must contain at least 1 capital letter, lower case letter, number, special character #?!@$%^&*- and be at least 8 characters long.",
+      },
+    };
+  }
+
+  return {
+    values: {
+      ...(values.firstName ? values : {}),
+      ...(values.lastName ? values : {}),
+      ...(validEmail ? values : {}),
+      ...(validPassword ? values : {}),
+    },
+    errors: errors,
+  };
+};
 
 export default function LoginForm() {
   const { register, handleSubmit, formState: { errors },} = useForm<FormValues>({resolver})
@@ -215,8 +258,11 @@ export default function LoginForm() {
       <input {...register("firstName")} placeholder="First Name" />
       {errors?.firstName && <p>{errors.firstName.message}</p>}
       <input {...register("lastName")} placeholder="Last Name"/>
+      {errors?.lastName && <p>{errors.lastName.message}</p>}
       <input type="email" {...register("email")} placeholder="Email"/>
+      {errors?.email && <p>{errors.email.message}</p>}
       <input type="password" {...register("password")} placeholder="Password"/>
+            {errors?.password && <p>{errors.password.message}</p>}
 
       <input type="submit" />
     </form>
--- client/src/utilities/helpers.ts
+++ client/src/utilities/helpers.ts
@@ -1,5 +1,17 @@
-export function checkPassword(submission: string) {
-    if (submission.length < 8) {
+export function validatePassword(submission: string) {
+    const regex =
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+    if (!regex.test(String(submission))) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+export function validateEmail(submission: string) {
+    const regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+
+    if (!regex.test(String(submission).toLowerCase())) {
         return false;
     } else {
         return true;
