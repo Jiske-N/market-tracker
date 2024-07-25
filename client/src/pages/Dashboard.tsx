e61f7c09517b3b3ceea7cae5d44e53b0c2f41835
@r0f33 <jiske.nicolson@gmail.com>
2024-07-25 15:59:50 +0930

    add form to add portfolios

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,9 +1,11 @@
+import AddPortfolioForm from '../components/AddPortfolioForm';
 import AddStockForm from '../components/AddStockForm';
 
 const Dashboard = () => {
     return (
         <>
         <AddStockForm/>
+        <AddPortfolioForm/>
         <h1>Dashboard</h1>
         </>
     )
