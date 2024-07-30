341f17f243b666846ba022238a44e85b6c6c13a4
@r0f33 <jiske.nicolson@gmail.com>
2024-07-30 16:18:35 +0930

    add background image to loginsignup pages

--- client/src/components/LoginSignupStructure.tsx
+++ client/src/components/LoginSignupStructure.tsx
@@ -3,14 +3,24 @@ import ColumnFlexJustifyAlignCenter from "./ColumnFlexJustifyAlignCenter";
 import LoginForm from "./LoginForm";
 import RowFlexBetweenCenter from "./RowFlexBetweenCenter";
 import SignUpForm from "./SignUpForm";
+import background from "../../public/login-signup.jpg";
 
 const LoginSignupStructure = () => {
     const currentPage: string = useLocation().pathname;
 
     return (
         <RowFlexBetweenCenter>
-            <ColumnFlexJustifyAlignCenter flexGrow="2.2" sx={{bgcolor: "background.alt"}}>
-                <h1>A cool background</h1>
+            <ColumnFlexJustifyAlignCenter
+                flexGrow="2.2"
+                sx={{
+                    backgroundImage: `url(${background})`,
+                    height: "100vh",
+                    width: "100%",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                }}
+            >
+                {/* <h1>A cool background</h1> */}
             </ColumnFlexJustifyAlignCenter>
             <ColumnFlexJustifyAlignCenter
                 flexGrow="1"
